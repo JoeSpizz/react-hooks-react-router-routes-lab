83c1936c1d86dc78fade4db16cb25b27068b966e
Joe <spizjo01@gmail.com>
2022-08-31 13:29:45 -0600

    commit tree 96aad405dd415799ef7911efe4b736a7e1cd36f1

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,35 @@ import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
+// src/
+// ├── data.js
+// ├── index.js
+// └── components/
+//     ├── App.js
+//     ├── Actors.js
+//     ├── Directors.js 
+//     ├── Home.js
+//     ├── Movies.js
+//     └── NavBar.js 
+
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  <NavBar/>
+  <Switch>
+    <Route exact path="/">
+    <Home/>
+    </Route>
+    <Route exact path="/movies">
+    <Movies/>
+    </Route>
+    <Route exact path="/actors">
+    <Actors/>
+    </Route>
+    <Route exact path="/directors">
+    <Directors/>
+    </Route>
+    </Switch>
+    </div>;
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,58 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+// Home, Movies, Actors, Directors, 
+const linkStyles = {
+  display: "inline-block",
+  width: "auto",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "red",
+  textDecoration: "none",
+  color: "green",
+};
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div className="navbar">
+  <NavLink
+  to="/"
+  exact
+  style={linkStyles}
+  activeStyle={{
+    background: "lightblue",
+    textDecoration: "wavy"
+  }}>
+  Home
+</NavLink>
+<NavLink
+  to="/movies"
+  exact
+  style={linkStyles}
+  activeStyle={{
+    background: "lightblue",
+    textDecoration: "wavy"
+  }}>
+  Movies
+</NavLink>
+<NavLink
+  to="/actors"
+  exact
+  style={linkStyles}
+  activeStyle={{
+    background: "lightblue",
+    textDecoration: "wavy"
+  }}>
+  Actors
+</NavLink>
+<NavLink
+  to="/directors"
+  exact
+  style={linkStyles}
+  activeStyle={{
+    background: "lightblue",
+    textDecoration: "wavy"
+  }}>
+  Directors
+</NavLink>
+</div>;
 }
 
 export default NavBar;
